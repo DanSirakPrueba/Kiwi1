a3cf791f766519cfb62566dbcd0b3eb3364b74e0
dansirak <dansirak@gmail.com>
2013-09-11 14:34:20 +0200

    Modificado Variables.java para adaptarlo a los Object

--- src/business/Variables.java
+++ src/business/Variables.java
@@ -17,12 +17,11 @@ public class Variables {
      *      @param name: The given name of the variable. 
      *      @param value: The given value of the variable.
      */
-    public static void createNewVariable(String orders) {
+    public static void createNewVariable(Object[] what) {
         // String name, String value
         // TODO: corregir el número.
-        String[] splitOrders = orders.split(" ");
-        String name = splitOrders[0];
-        String value = splitOrders[1];
+        String name = (String) what[0];
+        String value = (String) what[1];
         String phrase;
         phrase = "%^" + name + ".\"" + value + "\"";
         Deliver.deliver(Deliver.SYNTAX_AREA, phrase);
@@ -36,38 +35,37 @@ public class Variables {
 	 * @param type: Type of the variable, it can be w(word), s(string), d(number) or "..." which stands for any given text chain.
 	 * @param absolute: This boolean is set as true if the positioning is absolute and false if it is relative.
 	 */
-    public static void createVariableFromText(String orders) {
+    public static void createVariableFromText(Object[] what) {
         String phrase;
         //int column_start, int column_end, String name, String type, boolean absolute
         // TODO: La variable type --> w, s, d... (posible modificacion)
-        String[] splitOrders = orders.split(" ");
-        int column_start = Integer.parseInt(splitOrders[0]);
-        int column_end = Integer.parseInt(splitOrders[1]);
-        String name = splitOrders[2];
-        String type = splitOrders[3];
-        Boolean absolute;
-        absolute = (splitOrders[4].equalsIgnoreCase("1"))?true:false;
-		if (column_start == 0 && column_end == 255) {
-			if (absolute) {
-				phrase = "%-" + name + "." + type;
-			} else {
-				phrase = "%+" + name + "." + type;
-			}
+        
+	int column_start = (int) what[0];
+        int column_end = (int) what[1];
+        String name = (String) what[2];
+        String type = (String) what[3];
+        Boolean absolute = (boolean) what[4];
+       	if (column_start == 0 && column_end == 255) {
+		if (absolute) {
+			phrase = "%-" + name + "." + type;
+		} else {
+			phrase = "%+" + name + "." + type;
 		}
-		else if (column_start == 0 && column_end != 255) {
-			if (absolute) {
-				phrase = "%-" + column_end + name + "." + type;
-			} else {
-				phrase = "%+" + column_end + name + "." + type;
-			}
+	}
+	else if (column_start == 0 && column_end != 255) {
+		if (absolute) {
+			phrase = "%-" + column_end + name + "." + type;
+		} else {
+			phrase = "%+" + column_end + name + "." + type;
 		}
-		else {
-			if (absolute) {
-				phrase = "%" + column_start + "-" + column_end + name + "." + type;
-			} else {
-				phrase = "%" + column_start + "+" + column_end + name + "." + type;
-			}
+	}
+	else {
+		if (absolute) {
+			phrase = "%" + column_start + "-" + column_end + name + "." + type;
+		} else {
+			phrase = "%" + column_start + "+" + column_end + name + "." + type;
 		}
-		Deliver.deliver(Deliver.SYNTAX_AREA, phrase);
+	}
+	Deliver.deliver(Deliver.SYNTAX_AREA, phrase);
     }
 }
