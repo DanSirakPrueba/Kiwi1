b4f42408d2d678f1dcba319259e2cddb6c3032c2
Adrián <Adrin@Adrin-PC>
2013-09-24 13:04:19 +0200

    new things

--- src/business/FailureManager.java
+++ src/business/FailureManager.java
@@ -4,10 +4,35 @@
  */
 package business;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Adrián
  */
 public class FailureManager {
+    
+    
+    public ArrayList takeNewVariables(ArrayList list, String text){
+    
+        String[] varArry;
+        varArry = text.split("%");
+        
+        for(int i = 0; i < varArry.length;i++){
+            int cont = 0;
+            if(isLetter(varArry[i].charAt(cont))){
+                return null;
+            }
+            
+            
+        }
+        
+    
+        return list;
+    }
+    
+    public boolean isLetter(char letter){
+        return true;
+    }
     
 }
