6721f80c2a9fa92f356d0fff04357ae9ea279524
Adrián <Adrin@Adrin-PC>
2013-09-24 13:32:24 +0200

    procedimiento para conseguir las nuevas variables empezado

--- src/business/FailureManager.java
+++ src/business/FailureManager.java
@@ -12,18 +12,39 @@ import java.util.ArrayList;
  */
 public class FailureManager {
     
-    
+    /**
+     * 
+     * @param list
+     * @param text
+     * @return 
+     * 
+     * Check all Syntax text and get new variables.
+     */
     public ArrayList takeNewVariables(ArrayList list, String text){
     
         String[] varArry;
-        varArry = text.split("%");
+        varArry = text.split("%"); // % <- variable symbol.
         
-        for(int i = 0; i < varArry.length;i++){
-            int cont = 0;
-            if(isLetter(varArry[i].charAt(cont))){
-                return null;
-            }
+        for(int i = 0; i < varArry.length;i++){ // All possible variables.
             
+            boolean end = false;
+            while(!end){ // Recognise name of variable
+                int cont = 0;
+                if(isLetter(varArry[i].charAt(cont))){ // First character must be letter, end at a dot.
+                    end = true;
+                    int pointLocation = cont;
+                    
+                    while(varArry[i].charAt(pointLocation) == '.') // Find last character.
+                        pointLocation++;
+                    
+                    String newVar = varArry[i].substring(cont, pointLocation); // Get the name.
+                    
+                    if(!list.contains(newVar)) // Add if it's a new one.
+                        list.add(newVar);
+                        
+                }
+                cont++;
+            }
             
         }
         
@@ -31,7 +52,7 @@ public class FailureManager {
         return list;
     }
     
-    public boolean isLetter(char letter){
+    public boolean isLetter(char letter){ // Chech if it is a letter.
         return true;
     }
     
