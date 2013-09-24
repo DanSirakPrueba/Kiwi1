c6c45d48f2c210a06e1dfa452c1c7db3ea0ab7ef
Adrián <Adrin@Adrin-PC>
2013-09-24 12:32:18 +0200

    añadida nueva clase

---
+++ src/business/FailureManager.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package business;
+
+/**
+ *
+ * @author Adrián
+ */
+public class FailureManager {
+    
+}
